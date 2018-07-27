8bd30a6912b1554f1ee5d608e3f3fca6c50f6070
ritz-w <ritz.wu@outlook.com>
2018-07-27 16:40:51 +0100

    Done.

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,16 +1,33 @@
 class Gym
- ALL = []
+ @@all = []
 
   attr_reader :name
 
   def initialize(name)
     @name = name
-    ALL << self
+    @@all << self
 
   end
 
   def self.all
-    ALL
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|x| x.gym == self}
+  end
+
+  def lifters
+    self.memberships.map {|x| x.lifter}
+  end
+
+  def lifter_names
+    self.lifters.map{|x| x.name}
+  end
+
+  def lift_total_for_gym
+    total = self.lifters.map {|x| x.lift_total}
+    total.inject(:+)
   end
 
 end
--- app/models/lifter.rb
+++ app/models/lifter.rb
@@ -1,16 +1,41 @@
 
 class Lifter
 
-
-
   attr_reader :name, :lift_total
+
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select {|x| x.lifter == self}
+  end
+
+  def gyms
+    self.memberships.map {|x| x.gym}
+  end
+
+  def self.average_lift
+    lifts = self.all.map {|x| x.lift_total}
+    lifts.inject(:+)/lifts.size
+  end
+
+  def sign_up(cost, gym)
+    Membership.new(self, gym, cost)
+  end
+
+  def total_cost
+    costs = self.memberships.map {|x| x.cost}
+    costs.inject(:+)
+  end
 
 
 end
