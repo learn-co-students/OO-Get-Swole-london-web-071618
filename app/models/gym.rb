a53c33f7a86e28f063554185ac3f5bb970a58b33
github username <github email address>
2018-07-29 11:57:08 +0100

    done

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -3,14 +3,33 @@ class Gym
 
   attr_reader :name
 
-  def initialize(name)
-    @name = name
-    ALL << self
+      def initialize(name)
+        @name = name
+        ALL << self
 
-  end
+      end
 
-  def self.all
-    ALL
-  end
+      def self.all
+        ALL
+      end
+
+
+      def all_memberships
+        Membership.all.select {|membership| membership.gym == self}
+      end
+
+      def all_lifters
+        all_memberships.map {|membership| membership.lifter}
+      end
+
+      def all_lifters_names
+        all_lifters.map {|lifter| lifter.name}
+      end
+
+      def lift_total
+        lift_total = 0
+        all_lifters.each {|lifter| lift_total += lifter.lift_total}
+        lift_total
+      end
 
 end
--- app/models/lifter.rb
+++ app/models/lifter.rb
@@ -1,16 +1,46 @@
 
 class Lifter
 
-
-
   attr_reader :name, :lift_total
+
+
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
-
+    @@all << self
   end
 
+  def self.all
+      @@all
+  end
+
+  def memberships
+      Membership.all.select {|membership| membership.lifter == self}
+  end
+
+  def gyms
+    memberships = Membership.all.select {|membership| membership.lifter == self}
+    memberships.map {|membership| membership.gym}
+  end
+
+  def self.avg_lift_total
+      total = 0
+      Lifter.all.each {|lifter| total = total + lifter.lift_total}
+      total/ (Lifter.all.size)
+  end
+
+  def total_cost
+    memberships = Membership.all.select {|membership| membership.lifter == self}
+    total_cost = 0
+    memberships.each {|membership| total_cost += membership.cost}
+    total_cost
+  end
+
+  def add_membership(cost, gym)
+    Membership.new(cost, gym, self)
+  end
 
 
 end
