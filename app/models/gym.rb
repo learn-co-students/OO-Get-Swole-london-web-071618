4d9ab3c7e660bf3b56053e15403bff35c2292c31
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-07-30 19:35:28 +0100

    Done.

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -13,4 +13,22 @@ class Gym
     ALL
   end
 
+  def memberships
+    Membership.all.select {|m| m.gym == self}
+  end
+
+  def lifters
+    memberships.map {|l| l.cost}
+  end
+
+  def lifter_names
+    lifters.map {|n| n.name}
+  end
+
+  def lift_total_for_gym
+    total = lifters.map {|t| t.lift_total}
+    total.inject(0) {|sum,x| sum+x}
+  end
+
+
 end
